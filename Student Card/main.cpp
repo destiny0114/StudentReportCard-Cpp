8304324feb7befcd03083ca85e33e524c24c4922
Rockabye Saw <rockabyesaw@rockabyes-macbook-pro.local>
2019-12-21 14:04:53 +0800

    Initial Commit

---
+++ Student Card/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Student Card
+//
+//  Created by Rockabye Saw on 21/12/2019.
+//  Copyright © 2019 Rockabye Saw. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
